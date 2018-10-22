2e08f5af1ae5ed15cde941664e314e0c0ff3833e
Brandon Knitter <knitterb@blandsite.org>
2018-10-22 09:12:28 -0700

    add more debug for the script

--- modules/core_project_factory/main.tf
+++ modules/core_project_factory/main.tf
@@ -112,6 +112,9 @@ resource "null_resource" "delete_default_compute_service_account" {
     command = "ls -lsa ${path.module}/scripts/delete-service-account.sh"
   }
   provisioner "local-exec" {
+    command = "cat ${path.module}/scripts/delete-service-account.sh"
+  }
+  provisioner "local-exec" {
     command = "${path.module}/scripts/delete-service-account.sh ${local.project_id} ${var.credentials_path} ${data.google_compute_default_service_account.default.id}"
   }
 
